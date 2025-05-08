2f11fbea40b7772c299cb8febd42907f90ca0a29
nyrah-a <nyrahzmail@gmail.com>
2025-05-08 07:05:56 +0000

    Rename file to .py and add assignment code

---
+++ assignment1.py
@@ -0,0 +1,32 @@
+# Task 1 - Variables
+
+name = "Nyrah"
+age = 22
+height = 5.1
+
+print("Hi! My name is", name)
+print("I am", age, "years old")
+print("I am", height, "feet tall")
+
+# Task 2 - Operators
+
+num1 = 15
+num2 = 7
+
+print("Addition:", num1 + num2)
+print("Subtraction:", num1 - num2)
+print("Multiplication:", num1 * num2)
+print("Division:", num1 / num2)
+
+# Task 3 - Conditional Statements
+
+number = int(input("Enter a number: "))
+
+if number > 0:
+    print("This number is positive.")
+elif number < 0:
+    print("This number is negative.")
+else:
+    print("The number is zero.")
+
+    
