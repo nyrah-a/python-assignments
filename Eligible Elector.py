1f2110b0522c7d244f5a8477014050e1eaa78026
nyrah-a <nyrahzmail@gmail.com>
2025-05-08 00:47:22 -0700

    Eligible Elector.py

--- Eligible Elector.py
+++ Eligible Elector.py
@@ -1,1 +1,7 @@
-
+age = int(input("How old are you? "))
+if age >= 18:
+    print("🎉 Congratulations! You are eligible to vote. Go make a difference!")
+else:
+    years_left = 18 - age
+    year_word = "year" if years_left == 1 else "years"
+    print(f"⏳ Oops! You’re not eligible yet. But hey, only {years_left} more {year_word} to go!")
